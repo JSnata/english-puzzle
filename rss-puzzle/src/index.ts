7345f7997aab2eb697a37b0f63bda89b2fbcbeaa
natagetup <n.kulikouskaya@gmail.com>
2024-03-07 21:12:00 +0100

    chore: make project configuration

---
+++ rss-puzzle/src/index.ts
@@ -0,0 +1,2 @@
+console.log('hey');
+const abc = '';
---
+++ rss-puzzle/validate-branch-name.js
@@ -0,0 +1,8 @@
+const branchName = require('current-git-branch')();
+
+const isValidBranchName = /^feat\/[A-Z]+-[A-Z]+-\d+_.*$/.test(branchName);
+
+if (!isValidBranchName) {
+    console.error(`Invalid branch name: ${branchName}. Branch names must follow the specified format.`);
+    process.exit(1);
+}
