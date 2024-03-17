f2888e2f1002c3482cdaaf8cd4505fe25cb8ef3e
JSnata <149686160+JSnata@users.noreply.github.com>
2024-03-17 18:06:28 +0100

    feat: implement continue game button on the result page (#45)

--- src/components/resultsPage.ts
+++ src/components/resultsPage.ts
@@ -1,4 +1,26 @@
+import { saveGameProgress } from '../utils/localStorageUtils';
+import { state } from './app/app';
+import { renderGamePage } from './gamePage/gamePage';
+import { renderElement } from './renderElement';
+
+const continueGameButtonClickHandler = () => {
+  state.currentRoundNum += 1;
+  state.currentSentenceNum = 0;
+  state.resultArr = [];
+  state.sourceArr = [];
+  saveGameProgress();
+  renderGamePage(state.currentLevel, state.currentSentenceNum, state.currentRoundNum);
+};
+
 export const renderResultsPage = () => {
   const mainContainer = document.querySelector('.main-container') as HTMLElement;
   mainContainer.innerHTML = 'Results Page';
+
+  const resultContainer = renderElement('div', 'result-container', mainContainer);
+
+  const continueGameButton = renderElement('button', 'primary-button continue-game-button', resultContainer, {
+    innerText: 'Continue game',
+  }) as HTMLButtonElement;
+
+  continueGameButton.addEventListener('click', () => continueGameButtonClickHandler());
 };
